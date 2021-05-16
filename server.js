4c90e81f2b4172707359489f9ca888586d367eb4
Joshua <joshuamacleod@live.com>
2021-05-17 00:24:10 +0100

    api update

--- server.js
+++ server.js
@@ -1,37 +1,33 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors')
-const passport = require("passport");
-const path = require("path");
+ const cors = require('cors')
+ const passport = require("passport");
+ const users = require("./routes/api/users");
+ const path = require("path");
 
-const db = require('/Users/joshua/makers/thenewslab/news-server/db/index.js')
-const articleRouter = require('/Users/joshua/makers/thenewslab/news-server/routes/article-router.js')
-const users = require("/Users/joshua/makers/thenewslab/news-server/routes/api/users.js");
-
-
+ const db = require('./db')
+ const articleRouter = require('./routes/article-router')
 const app = express()
 const port = process.env.PORT || 5000;
-
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
-app.use(passport.initialize());
-app.use(express.static(path.join(__dirname, "client", "build")))
+ app.use(cors())
+ app.use(bodyParser.json())
+ app.use(passport.initialize());
+ app.use(express.static(path.join(__dirname, "client", "build")))
 
 
-require("/Users/joshua/makers/thenewslab/news-server/config/passport.js")(passport);
+ require("./config/passport")(passport);
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+   res.send('Hello World!')
+ })
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "news-front", "build", "index.html"));
-});
+ app.get("*", (req, res) => {
+     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+ });
 
-app.use('/Users/joshua/makers/thenewslab/news-server/routes/article-router.js', articleRouter)
-app.use("/Users/joshua/makers/thenewslab/news-server/routes/api/users.js", users);
+ app.use('/api', articleRouter)
+ app.use("/api/users", users);
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`))
